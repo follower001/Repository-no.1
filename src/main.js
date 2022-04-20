eca639a9ab54c72da1c1944df9ace8f994bfd3b2
user3184 <735647220@yozosoft.com>
2022-04-20 15:57:41 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import "./styles/global.less"  //导入全局样式
+
+// import styles from "./styles/message.module.less"
+// console.log(styles);
+// const div = document.createElement("div");
+// div.className = styles.message;
+// document.body.appendChild(div);
+// div.innerText = "6966";
+
+//得到组件生成的根dom 
+function getComponentRootDom(comp,props){
+    const vm = new Vue({
+      render:(h) => h(comp,{props}),
+    });
+    vm.$mount();
+    return vm.$el;
+}
+
+import Icon from "./components/Icon"
+
+ var dom = getComponentRootDom(Icon,{type:"icon-home"})
+console.log(dom);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
